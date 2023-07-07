70c2673d259223d704b4d8a5fb3e0fa121a0d70a
GeorgeKlemperer <george@klemperer.co.uk>
2023-07-07 10:07:59 +0100

    word display test

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Function to get a single random 5-letter word
 async function getRandomWord() {
+  // API endpoint URL
   const apiUrl = 'https://random-word-api.vercel.app/api?words=1&length=5';
 
   try {
+    // Fetch data from the API
     const response = await fetch(apiUrl);
     const data = await response.json();
     
+    // Check if the response is successful
     if (response.ok) {
       const randomWord = data[0];
       console.log(randomWord);
@@ -16,4 +20,5 @@ async function getRandomWord() {
   }
 }
 
+// Call the function to get a random word
 getRandomWord();
